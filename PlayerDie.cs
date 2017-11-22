015ac59b51a29a3c5fc786ed62dbe6c84285f556
LucasHilgevoord <lucashilgevoordd@gmail.com>
2017-11-22 22:32:57 +0100

    Lucas Scripts

---
+++ PlayerDie.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDie : MonoBehaviour
+{
+    public ParticleSystem Explosion;
+    public GameObject PlayerModel;
+    public static bool IsAlive = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            Die();
+        }
+    }
+    
+    void Die()
+    {
+        Explosion.Play();
+        Debug.Log("Hit!");
+        PlayerMovement.speed = 0.0f;
+        Destroy(PlayerModel);
+        IsAlive = false;
+        StartCoroutine("Wait");
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(4);
+        Application.LoadLevel(0);
+    }
+}
